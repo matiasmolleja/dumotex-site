6c44626f6cd623b5bdc69e351546b3f20d73e8f2
Matías Molleja <matiasmolleja@outlook.com>
2020-05-17 22:48:16 +0200

    Fix that static assets where in the wrong directory.

--- dumotex-site/pages/homepage.vue
+++ dumotex-site/pages/homepage.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-            <!-- start header -->
+        <!-- start header -->
         <header>
             <!-- start navigation -->
             <nav class="navbar navbar-default bootsnav bg-transparent navbar-top header-dark white-link">
@@ -543,10 +542,48 @@
             <!-- end navigation -->
         </header>
         <!-- end header -->
-    <nuxt />
-  </div>
 </template>
 
-<style>
+<script lang="ts">
+import Vue from 'vue'
+import Logo from '~/components/Logo.vue'
 
+export default Vue.extend({
+  components: {
+    Logo
+  }
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
 </style>
